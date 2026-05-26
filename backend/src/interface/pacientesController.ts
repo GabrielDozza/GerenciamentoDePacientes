b5090bac1bfc024afcc2066a89d948df079442e3
BaltazarDostatny <dostatny.dev@gmail.com>
2026-05-26 14:17:11 -0300

    Fix Prisma

--- backend/src/interface/pacientesController.ts
+++ backend/src/interface/pacientesController.ts
@@ -1,6 +1,13 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get } from "@nestjs/common";
+import { getPacientes } from "../persistence/pacientes";
 
 @Controller("pacientes")
 export class pacientesController {
 
+    @Get()
+    getPacientes() {
+
+        const pacientes = getPacientes();
+        return pacientes;
+    };
 };
