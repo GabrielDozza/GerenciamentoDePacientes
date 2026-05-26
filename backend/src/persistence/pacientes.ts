d2b73ea56c524bea41fc5e516f0764978bb3a70f
BaltazarDostatny <dostatny.dev@gmail.com>
2026-05-26 12:16:30 -0300

    Add persistence

---
+++ backend/src/persistence/pacientes.ts
@@ -0,0 +1,59 @@
+import prisma from "../../prisma/prisma.js";
+
+async function getPacientes() {
+    const pacientes = await prisma.paciente.findMany({});
+    return pacientes;
+};
+
+async function getPacientesId(id: String) {
+    const paciente = await prisma.paciente.findFirst({
+        where: { id: Number(id) }
+    });
+
+    return paciente;
+};
+
+async function postPaciente(body: any) {
+    const paciente = await prisma.paciente.create({
+        data: {
+            nome: body.nome,
+            dataNascimento: body.dataNascimento,
+            telefone: body.telefone,
+            email: body.email,
+            cpf: body.cpf,
+            endereco: body.endereco,
+            profissao: body.profissao,
+            origem: body.origem,
+            eventos: body.eventos,
+            evolucoes: body.evolucoes
+        }
+    })
+
+    return paciente
+};
+
+async function patchPaciente(id: String, body: any) {
+    const paciente = await prisma.paciente.update({
+        where: {
+            id: Number(id)
+        },
+
+        data: {
+            ...body
+        }
+    });
+
+    return paciente;
+};
+
+async function deletePaciente(id: String) {
+    const paciente = await prisma.paciente.delete({
+        where: {
+            id: Number(id)
+        }
+    })
+
+    return paciente;
+};
+
+export { getPacientes, getPacientesId, postPaciente, patchPaciente, deletePaciente }
